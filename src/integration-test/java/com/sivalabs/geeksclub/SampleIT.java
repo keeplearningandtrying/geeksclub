d437bcebbfd493ec64ac47651088cd9daa0b6ab1
K Siva Prasad Reddy <sivaprasadreddy.k@gmail.com>
2017-07-20 18:18:36 +0530

    Adding separate integration-test folders.

---
+++ src/integration-test/java/com/sivalabs/geeksclub/SampleIT.java
@@ -0,0 +1,14 @@
+package com.sivalabs.geeksclub;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class SampleIT
+{
+    @Test
+    public void dummyIntegrationTest()
+    {
+        assertTrue(true);
+    }
+}
